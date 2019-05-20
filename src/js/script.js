70473d534cf2ca797713f84c1f19f522e3a202af
Andrey <soul.evans.15@gmail.com>
2019-05-20 15:04:04 +0300

    finished main-content, added first menu-toggle animation

--- src/js/script.js
+++ src/js/script.js
@@ -24,4 +24,7 @@ $(function () {
         else
             video.pause();
     });
+    $('.menu-toggler').click(function () {
+        $('.outer-container').toggleClass('menu-open');
+    });
 });
