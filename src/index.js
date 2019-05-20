85f14f00703529d0c6332308c0f2bb7ced29fc1c
Andrey <soul.evans.15@gmail.com>
2019-05-20 22:51:31 +0300

    finished popup, added crossbrowser prefixes in css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 import './css/style.css';
 import './js/script';
-import './public/comp-logo.png';
-import './public/bg.mp4';
--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,14 @@ import 'owl.carousel2/dist/assets/owl.carousel.css';
 import $ from 'jquery';
 import 'owl.carousel2/dist/owl.carousel.min';
 $(function () {
+    // background slider
     let bgCarousel = $('.bg-slider');
     bgCarousel.owlCarousel({
         items: 1,
         mouseDrag: false,
 
     });
+    // play/pause background slider
     $('.play-button').click(function (event) {
         let video;
         if (event.target.classList[0] === 'play-button') {
@@ -24,12 +26,32 @@ $(function () {
         else
             video.pause();
     });
+    // open/close menu
     $('.menu-toggler').click(function () {
         $('.outer-container').toggleClass('menu-open');
     });
     $('.menu-closer').click(function () {
         if ($('.outer-container').hasClass('menu-open')) {
             $('.outer-container').removeClass('menu-open');
+        }
+    });
+    // registration popup
+    $('.register').click(function () {
+        $('.popup-registration').fadeIn(200);
+    });
+    $('.popup-closer').click(function () {
+        $('.popup-registration').fadeOut(200);
+    });
+    $('.popup-registration').click(function (event) {
+        if ($(event.target).hasClass('popup-registration')) {
+            $('.popup-registration').fadeOut(200);
+        }
+    });
+    $('.form-switch').click(function (event) {
+        let target = $(event.target);
+        if (!target.hasClass('active')) {
+            $('.form-switch.active').removeClass('active');
+            target.addClass('active');
         }
     });
 });
